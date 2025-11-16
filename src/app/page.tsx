5c77777a4afabbee9cc45e560c2eca634ae16d37
elianriu <sinagadarvin@gmail.com>
2025-11-16 11:36:29 +0700

    feat: remove personal data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,18 @@ export default function Home() {
             <div className="mt-5">Experienced  <b>Frontend Developer</b> adept in bringing forth expertise in design, installation, testing and maintenance of software systems.  Have been doing a lot of react native app development. Apart from my own forum, I have worked remotely with some of <b>Singapore's</b> start-ups</div>
 
             <div className="mt-5">
-                I am currently Worked at <a href="https://dbs.com"><b>DBS Bank</b></a> as Backend Developer
+                I am currently Worked at <a href=""><b>Banking Company</b></a> as Backend Developer
             </div>
             <div className="mt-5">
-                I am currently a Founder of <a href="https://garar.id"><b>Garar ID</b></a> Maintaining a database of 200 digital products, such as game vouchers, credit, electricity tokens
+                Previously a Founder of <a href=""><b>[Redacted App]</b></a> Maintaining a database of 200 digital products, such as game vouchers, credit, electricity tokens
             </div>
 
             <div className="mt-5">
-                Previously, I have worked as a Frontend Developer at <b>ProSpace</b>, and <b>Rushowl Singapore</b>
+                And I have worked as a Frontend Developer at <b>ProSpace</b>, and <b>Rushowl Singapore</b>
             </div>
             
             <div className="flex mt-5 text-blue-500">
                 <a className="ml-0" target="_blank" rel="noopener noreferrer" href="https://github.com/elianriu">github</a>
-                <a className="ml-10" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/elianriu/">linkedin</a>
                 <a className="ml-10" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@elianriu">youtube</a>
                 
             </div>
